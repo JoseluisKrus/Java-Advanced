d94ff03d502f0e06787f0df6e19fcf33ef1d1ee8
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-11 12:13:18 +0300

    Update main.java

--- 01. Stacks and Queues/Lab/03. Decimal To Binary/main.java
+++ 01. Stacks and Queues/Lab/03. Decimal To Binary/main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int decimal = Integer.parseInt(scanner.nextLine());
