2ade1508abfb8f134d3c0ca1cc06f9d3fae8ee84
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-11 11:38:39 +0300

    create main.java

---
+++ 01. Stacks and Queues/Lab/01. Browser History/main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String currentURL = "";
+        ArrayDeque <String> history = new ArrayDeque<>();
+        while (!command.equals("Home")){
+            if (command.equals("back")){
+                if (history.isEmpty()) {
+                    System.out.println("no previous URLs");
+                    command = scanner.nextLine();
+                    continue;
+                }
+                String prevURL= history.pop();
+                currentURL= prevURL;
+
+            }else {
+                if (!currentURL.equals("")) {
+                    history.push(currentURL);
+                }
+                currentURL = command;
+            }
+            System.out.println(currentURL);
+            command = scanner.nextLine();
+
+        }
+    }
+}
