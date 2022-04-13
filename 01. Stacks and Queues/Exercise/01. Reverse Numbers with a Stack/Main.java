e95cd88ee7d2fbbb50cd4dfa03b8a3e3f2869594
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-13 18:18:02 +0300

    Create Main.java

---
+++ 01. Stacks and Queues/Exercise/01. Reverse Numbers with a Stack/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] numbers = scanner.nextLine().split(" ");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        for (String number :numbers) {
+            stack.push(number);
+        }
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop()+ " ");
+        }
+    }
+}
