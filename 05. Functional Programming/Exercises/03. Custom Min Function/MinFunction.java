b2cb5782105cf780b70452357dfb292bdf3ba260
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-24 11:01:19 +0300

    Create MinFunction.java

---
+++ 05. Functional Programming/Exercises/03. Custom Min Function/MinFunction.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class MinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int [] numbers = Arrays.stream(input.split("\\s+")).mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        Consumer<int[]> printMin = array -> System.out.println(Arrays.stream(array).min().orElse(0));
+        printMin.accept(numbers);
+    }
+}
