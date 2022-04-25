bf99ecb3176825f0c9991c15773cccaeb0bcc4b8
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-25 20:00:08 +0300

    Create Engine.java

---
+++ 06. Defining classes/Exercises/04. Raw Data/Engine.java
@@ -0,0 +1,20 @@
+public class Engine {
+    private int speed;
+    private int power;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+}
