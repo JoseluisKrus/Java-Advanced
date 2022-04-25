6f7ffc64b8c796c5d316c6262ccb54df316b2ddd
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-25 19:59:46 +0300

    Create Cargo.java

---
+++ 06. Defining classes/Exercises/04. Raw Data/Cargo.java
@@ -0,0 +1,20 @@
+public class Cargo {
+    private String type;
+    private int weight;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
