6c0ebfae0f5c783f60a2d9083e5b437d374b9563
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-26 15:00:14 +0300

    Create Main.java

---
+++ 06. Defining classes/Lab/01. Car Info/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i <n; i++) {
+            String [] input = scanner.nextLine().split(" ");
+            Car car = new Car();
+            car.setBrand(input[0]);
+            car.setModel(input[1]);
+            car.setHorsePower(Integer.parseInt(input[2]));
+            System.out.println(car.carInfo());
+        }
+
+    }
+
+}
