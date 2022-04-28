58479ef0657050c7efcf6fef1c32f6046db0780b
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-04-28 12:49:00 +0300

    Create ArrayCreator.java

---
+++ 07. Generics/Lab/02. Generic Array Creator/ArrayCreator.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.stream.IntStream;
+
+public class ArrayCreator {
+    @SuppressWarnings(("unchecked"))
+    public static <T> T[] create(int length, T item){
+        T[] arr =(T[]) Array.newInstance(item.getClass(), length);
+
+        IntStream.range(0, length).forEach(i ->arr[i] = item);
+        return arr;
+    }
+    @SuppressWarnings(("unchecked"))
+    public static <T> T[] create(Class<T> clazz, int length, T value){
+        T[] arr = (T[])Array.newInstance(clazz, length);
+        IntStream.range(0, length)
+                .forEach(i -> arr[i] = value);
+        return arr;
+    }
+
+}
